09a7a7be4a78cfc4a2a74e6d1b37a683f0832adc
Marcx <haitianhero6@gmail.com>
2022-10-18 16:51:19 -0400

    interfaces

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,12 @@
+async function main() {
+  //protocol treats everything as an ERC20 token
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/getWeth.js
@@ -0,0 +1,9 @@
+const { getNamedAccounts } = require("hardhat");
+
+async function getWeth() {
+  const { deployer } = await getNamedAccounts;
+}
+
+module.exports = {
+  getWeth,
+};
